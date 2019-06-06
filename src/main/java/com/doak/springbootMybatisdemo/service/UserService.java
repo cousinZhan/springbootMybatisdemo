38dccb8fe9d112aacc1101bdc09bed9e30d169d0
cousinZhan <18813973822@163.com>
2019-06-06 15:56:36 +0800

    我的第一个spring boot mybatis

---
+++ src/main/java/com/doak/springbootMybatisdemo/service/UserService.java
@@ -0,0 +1,21 @@
+package com.doak.springbootMybatisdemo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.doak.springbootMybatisdemo.entity.User;
+import com.doak.springbootMybatisdemo.mapper.UserMapper;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	UserMapper userMapper;
+	
+	
+	public User selectUser(User user){
+		return  userMapper.selectUser(user.getUserName(), user.getPassword());
+	}
+
+	
+}
